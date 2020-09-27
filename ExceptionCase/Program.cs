0cbaa7f6b105059a85810d2006f4afcbddac74c5
vmakeev <unclearnuclear@yahoo.com>
2020-09-27 15:18:27 -0700

    Add project files.

---
+++ ExceptionCase/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExceptionsLibrary;
+using System;
+
+namespace ExceptionCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoCode demo = new DemoCode();
+            try
+            {
+                int result = demo.GrandParentMethod(4);
+                Console.WriteLine($"The value at the given position is {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsLibrary
+{
+   public class DemoCode
+    {
+
+        public int GrandParentMethod(int position)
+        {
+            int output = 0;
+            Console.WriteLine("Open Database Connection");
+
+            try
+            {
+                output = ParentMethod(position);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Close Database Connection");
+            }
+            return output;
+        }
+
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+           // try
+           //{
+                int[] numbers = new int[] { 1, 4, 7, 2 };
+                return numbers[position];
+                /*  }
+                  catch (Exception ex)
+                  {
+                      Console.WriteLine(ex.Message);
+                      Console.WriteLine(ex.StackTrace);
+                  }*/
+                return output;
+
+        }
+    }
+}
+  
